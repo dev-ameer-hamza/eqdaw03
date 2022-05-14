a761dea8a6220866e3fb08bca5a73fec22040acb
dev-ameer-hamza <ameer.hamza@ikasle.egibide.org>
2022-05-14 23:42:37 +0200

    Codificacion de la clase PersonaDAO

---
+++ fase3/java/src/BD/UML/PersonaDAO.java
@@ -0,0 +1,66 @@
+package BD.UML;
+
+import Modelo.Persona;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonaDAO {
+    private Connection conn;
+
+    /**
+     * Creamos el constructor para clase PersonaDAO
+     * @param conn
+     */
+    public PersonaDAO(Connection conn)
+    {
+        this.conn = conn;
+    }
+
+    /**
+     * Creamos el metodo para insertar los datos de la persona a la base de datos
+     * @param p objeto persona
+     * @return ResultSet
+     * @throws SQLException
+     */
+    public ResultSet crearPersona(Persona p) throws SQLException {
+        PreparedStatement pst = conn.prepareStatement("insert into persona values(?,?)");
+        pst.setString(1,p.getNombre());
+        pst.setString(2,p.getApellido());
+
+        ResultSet datosPersona = pst.executeQuery();
+        return datosPersona;
+    }
+
+    /**
+     * Creamos el metodo para borrar una persona
+     * @param id
+     * @return boolean
+     * @throws SQLException
+     */
+    public boolean borrarPersona(int id) throws SQLException {
+        PreparedStatement pst = conn.prepareStatement("delete from persona where id_persona=?");
+        pst.setInt(1,id);
+        int resultado = pst.executeUpdate();
+        return resultado == 1;
+    }
+
+    /**
+     * Creamos el metodo para modificar los datos de la persona
+     * @param p
+     * @param id
+     * @return ResultSet
+     * @throws SQLException
+     */
+    public ResultSet modificarPersona(Persona p,int id) throws SQLException {
+        PreparedStatement pst = conn.prepareStatement("update persona set nombre=?,apellido=? where id_persona=?");
+        pst.setString(1,p.getNombre());
+        pst.setString(2,p.getApellido());
+        pst.setInt(2,id);
+        return pst.executeQuery();
+    }
+
+    // para consultar personas debemos crear una vista
+}
