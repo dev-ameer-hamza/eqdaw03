a55922b3337915556887259cf31bdb3e59a5d696
Ameer Hamza <ameer.hamza@ikasle.egibide.org>
2022-05-16 14:08:03 +0200

    conexion entre interfaz y codigo

--- fase3/java/src/Vistas/Login/VentanaLogin.java
+++ fase3/java/src/Vistas/Login/VentanaLogin.java
@@ -5,11 +5,13 @@ import com.company.Main;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VentanaLogin {
     private JPanel panelLogin;
     private JTextField tfUsuarioLogin;
-    private JPasswordField tfContraseñaLogin;
+    private JPasswordField tfContrasenyaLogin;
     private JButton siguienteButton;
 
     public VentanaLogin() {
@@ -19,7 +21,45 @@ public class VentanaLogin {
                 Main.mostrarVentanaAdmin();
             }
         });
+        siguienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    verificarDatos(tfUsuarioLogin.getText(), tfContrasenyaLogin.getPassword());
+                } catch (Exception ex) {
+                    mostrarError(ex.getMessage());
+                }
+            }
+        });
     }
 
     public JPanel getPanelLogin() {return panelLogin;}
+
+    public boolean verificarDatos(String usuario, char[] contrasenya) throws Exception {
+
+        if (usuario.isEmpty() || contrasenya.toString().isEmpty()) {
+
+            throw new Exception("Usuario o contraseña estan vacios");
+
+        }
+
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]+$");
+        Matcher matcher = pattern.matcher(usuario);
+
+        if(!matcher.matches()){
+            throw  new Exception("El formato de usuario no es valido");
+        }
+
+
+
+        return true;
+    }
+
+    public void mostrarError(String mensaje){
+
+        JOptionPane.showMessageDialog(null,mensaje);
+
+    }
+
+
 }
